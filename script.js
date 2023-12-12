6f9cad020b111ee8506ed8e8ed5f1052edac8181
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-12 19:39:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let ans=0;
+	for(let i=0;i<str.length;i++)
+		{
+			let num=str.charCodeAt(i);
+			ans^=num;
+		}
+	return ans==0?null:String.fromCharCode(ans);
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
