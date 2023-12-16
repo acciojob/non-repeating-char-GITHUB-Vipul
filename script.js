178b51e6b2d57b7a5cc40b11e05d8389bfb201cb
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-16 19:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj={};
 	let ans=0;
 	for(let i=0;i<str.length;i++)
 		{
-			let num=str.charCodeAt(i);
-			ans^=num;
+			let char=str.charCodeAt(i);
+			if(obj[char]==undefined)
+			{
+				obj[char]=1;
+			}
+			else
+			{
+				let count=obj[char];
+				obj[char]=count+1;
+			}
+		}
+
+	for(let key in obj)
+		{
+			if(obj[key]===1)
+			{
+				ans=key;
+			}
 		}
 	return ans==0?null:String.fromCharCode(ans);
 
